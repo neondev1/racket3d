9005a7e9b62453928a88234bc62de3196cd4b408
neondev1 <82016241+neondev1@users.noreply.github.com>
2025-10-10 00:15:23 -0700

    Minor changes to data definitions

--- racket3d.rkt
+++ racket3d.rkt
@@ -50,20 +50,21 @@
        (euler-beta  e)
        (euler-gamma e)))
 
-
 (@htdd Triangle)
-(define-struct tri (v0 v1 v2))
-;; Triangle is (make-tri Point Point Point)
-;; interp. the three vertices of a triangle
+(define-struct tri (v0 v1 v2 col))
+;; Triangle is (make-tri Point Point Point Color)
+;; interp. the three vertices and fill colour of a triangle
 ;; CONSTRAINT: No two vertices should be equal
 (define T1 (make-tri (make-point 0 1 1)
                      (make-point 1 0 1)
-                     (make-point 0 0 1))) ;triangles for a
+                     (make-point 0 0 1)
+                     "black"))            ;triangles for a
 (define T2 (make-tri (make-point 0 1 1)   ;rectangular mesh
                      (make-point 1 1 1)
-                     (make-point 1 0 1)))
+                     (make-point 1 0 1)
+                     "black"))
 
-(@dd-template-rules compound ;3 fields
+(@dd-template-rules compound ;4 fields
                     ref      ;(tri-v0 Triangle) is Point
                     ref      ;(tri-v1 Triangle) is Point
                     ref)     ;(tri-v2 Triangle) is Point
@@ -71,81 +72,89 @@
 (define (fn-for-triangle t)
   (... (fn-for-point (tri-v0 t))
        (fn-for-point (tri-v1 t))
-       (fn-for-point (tri-v2 t))))
+       (fn-for-point (tri-v2 t))
+       (tri-col t)))
 
 ;;
 ;; INTERNAL DEFINITIONS
 ;;
 
 (@htdd Cuboid)
-(define-struct cuboid (pos rot w h d))
-;; Cuboid is (make-cuboid Point Euler Number Number Number)
-;; interp. the position, orientation, width, height and depth of a cuboid
+(define-struct cuboid (pos rot xs ys zs col))
+;; Cuboid is (make-cuboid Point Euler Number Number Number Color)
+;; interp. the position, orientation, x, y, z scales and colour of a cuboid
 (define C1 (make-cuboid (make-point 0 0 0)
                         (make-euler 0 0 0)
-                        1 1 1)) ;Unit cube
+                        1 1 1 "black")) ;Unit cube
 (define C2 (make-cuboid (make-point 1 2 3)
                         (make-euler 0 0 0)
-                        2 4 6))
+                        2 4 6 "black"))
 (define C3 (make-cuboid (make-point 1 2 1)
                         (make-euler 45 45 45)
-                        2 3 4))
+                        2 3 4 "black"))
 
-(@dd-template-rules compound ;5 fields
+(@dd-template-rules compound ;6 fields
                     ref      ;(cuboid-pos Cuboid) is Point
                     ref)     ;(cuboid-rot Cuboid) is Euler
 
 (define (fn-for-cuboid c)
   (... (fn-for-point (cuboid-pos c))
        (fn-for-euler (cuboid-rot c))
-       (cuboid-w c)
-       (cuboid-h c)
-       (cuboid-d c)))
+       (cuboid-xs c)
+       (cuboid-ys c)
+       (cuboid-zs c)
+       (cuboid-col c)))
 
 
-(@htdd Ellipsoid)
-(define-struct ellipsoid (pos rot xs ys zs))
-;; Ellipsoid is (make-ellipsoid Point Euler Number Number Number)
-;; interp. the position, orientation, and scales of an ellipsoid
-(define E1 (make-ellipsoid (make-point 0 0 0)
+(@htdd Icosphere)
+(define-struct icosphere (pos rot xs ys zs col))
+;; Icosphere is (make-icosphere Point Euler Number Number Number Color)
+;; interp. the position, orientation, x, y, z scales and colour of an icosphere
+(define E1 (make-icosphere (make-point 0 0 0)
                            (make-euler 0 0 0)
-                           1 1 1)) ;sphere
-(define E2 (make-ellipsoid (make-point 0 0 0)
+                           1 1 1 "black")) ;sphere
+(define E2 (make-icosphere (make-point 0 0 0)
                            (make-euler 23 37 79)
-                           1 1 1)) ;rotating sphere has no effect
-(define E3 (make-ellipsoid (make-point 1 3 5)
+                           1 1 1 "black")) ;rotated sphere is nearly identical
+(define E3 (make-icosphere (make-point 1 3 5)
                            (make-euler 100 120 140)
-                           3 4 5))
+                           3 4 5 "black"))
 
-(@dd-template-rules compound ;5 fields
-                    ref      ;(ellipsoid-pos Ellipsoid) is Point
-                    ref)     ;(ellipsoid-rot Ellipsoid) is Euler
+(@dd-template-rules compound ;6 fields
+                    ref      ;(icosphere-pos Icosphere) is Point
+                    ref)     ;(icosphere-rot Icosphere) is Euler
 
-(define (fn-for-ellipsoid e)
-  (... (fn-for-point (ellipsoid-pos e))
-       (fn-for-euler (ellipsoid-rot e))
-       (ellipsoid-xs e)
-       (ellipsoid-ys e)
-       (ellipsoid-zs e)))
+(define (fn-for-icosphere i)
+  (... (fn-for-point (icosphere-pos i))
+       (fn-for-euler (icosphere-rot i))
+       (icosphere-xs i)
+       (icosphere-ys i)
+       (icosphere-zs i)
+       (icosphere-col i)))
 
 
 (@htdd Mesh)
 ;; Mesh is one of:
 ;;  - empty
 ;;  - (cons Triangle Mesh)
+;; interp. a mesh composed of triangular faces
 (define M1 empty)
 (define M2 (list (make-tri (make-point 2 0 0) ;Tetrahedron mesh
                            (make-point -1 -1 (/ (sqrt 13) 2))
-                           (make-point -1 -1 (/ (sqrt 13) -2)))
+                           (make-point -1 -1 (/ (sqrt 13) -2))
+                           "black")
                  (make-tri (make-point -1 2 0)
                            (make-point -1 -1 (/ (sqrt 13) 2))
-                           (make-point -1 -1 (/ (sqrt 13) -2)))
+                           (make-point -1 -1 (/ (sqrt 13) -2))
+                           "black")
                  (make-tri (make-point 2 0 0)
                            (make-point -1 2 0)
-                           (make-point -1 -1 (/ (sqrt 13) 2)))
+                           (make-point -1 -1 (/ (sqrt 13) 2))
+                           "black")
                  (make-tri (make-point 2 0 0)
                            (make-point -1 2 0)
-                           (make-point -1 -1 (/ (sqrt 13) -2)))))
+                           (make-point -1 -1 (/ (sqrt 13) -2))
+                           "black")))
 
 (@dd-template-rules one-of          ;2 cases
                     atomic-distinct ;empty
@@ -167,7 +176,7 @@
 (@htdd Object)
 ;; Object is one of:
 ;;  - Cuboid
-;;  - Ellipsoid
+;;  - Icosphere
 ;;  - Mesh
 ;; interp. the position, orientation and size info of an object
 (define O1 C1)
@@ -177,7 +186,7 @@
 (@dd-template-rules one-of   ;3 cases
                     compound ;Cuboid
                     ref
-                    compound ;Ellipsoid
+                    compound ;Icosphere
                     ref
                     compound ;Mesh
                     ref)
@@ -185,8 +194,8 @@
 (define (fn-for-object o)
   (cond [(cuboid? o)
          (... (fn-for-cuboid o))]
-        [(ellipsoid? o)
-         (... (fn-for-ellipsoid o))]
+        [(icosphere? o)
+         (... (fn-for-icosphere o))]
         [else
          (... (fn-for-mesh o))]))
 
@@ -315,6 +324,38 @@
                (* (vec-x v1) (vec-y v0)))))
 
 
+(@htdf dot)
+(@signature Vector Vector -> Number)
+;; produce dot product of given vectors
+(check-expect (dot (make-vec 2 0 0)
+                   (make-vec 0 2 2))
+              0)
+(check-expect (dot (make-vec 0 2 0)
+                   (make-vec 0 3 0))
+              6)
+(check-expect (dot (make-vec 1.2 3.4 5.6)
+                   (make-vec 9.8 -7.6 5.4))
+              16.16)
+
+;(define (dot v0 v1) 0) ;stub
+
+(@template-origin Vector)
+
+(@template
+ (define (dot v0 v1)
+   (... (vec-x v0)
+        (vec-y v0)
+        (vec-z v0)
+        (vec-x v1)
+        (vec-y v1)
+        (vec-z v1))))
+
+(define (dot v0 v1)
+  (+ (* (vec-x v0) (vec-x v1))
+     (* (vec-y v0) (vec-y v1))
+     (* (vec-z v0) (vec-z v1))))
+
+
 (@htdf mag)
 (@signature Vector -> Number)
 ;; produce magnitude of given vector
@@ -370,32 +411,33 @@
 
 (@htdf normal)
 (@signature Triangle -> Vector)
-;; produce unit vector normal to given triangle
+;; produce vector normal to given triangle
 ;; CONSTRAINT: given triangle must be non-degenerate
 (check-expect (normal (make-tri (make-point 0 0 0)
                                 (make-point 2 0 0)
-                                (make-point 0 2 0)))
-              (make-vec 0 0 1))
+                                (make-point 0 2 0)
+                                "black"))
+              (make-vec 0 0 4))
 (check-expect (normal (make-tri (make-point 0 0 0)
                                 (make-point 0 2 0)
-                                (make-point 2 0 0)))
-              (make-vec 0 0 -1))
+                                (make-point 2 0 0)
+                                "black"))
+              (make-vec 0 0 -4))
 
 ;(define (normal t) (make-vec 0 0 0)) ;stub
 
 (@template-origin fn-composition)
 
 (define (normal t)
-  (div (cross (to-vec (tri-v1 t) (tri-v0 t))
-              (to-vec (tri-v2 t) (tri-v1 t)))
-       (mag (cross (to-vec (tri-v1 t) (tri-v0 t))
-                   (to-vec (tri-v2 t) (tri-v1 t))))))
+  (cross (to-vec (tri-v1 t) (tri-v0 t))
+         (to-vec (tri-v2 t) (tri-v1 t))))
 
 ;;
 ;; WORLD
 ;;
 
 (@htdd Camera)
+
 
 (@htdf main)
 (@signature Camera -> Camera)
