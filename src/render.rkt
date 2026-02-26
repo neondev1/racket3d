499e5714a646f5449e1e02afbfc1b12e098b74c0
neondev1 <82016241+neondev1@users.noreply.github.com>
2026-02-26 11:28:03 -0800

    Update buffer procedure again

--- src/render.rkt
+++ src/render.rkt
@@ -35,17 +35,17 @@ Phase 1 - merging element and vertex buffers; for each mesh face added to the
 2. Check if the two triangles intersect.
    2a. Transform one triangle into a basis B formed by two edges of the other
        triangle as well as their cross product, with the origin at its v0.
-   2b. Consider each edge of the first triangle as a parametric line; call one
-       such line v. Compute the point at which the z-component of [v]_B is 0.
+   2b. Consider each edge of the first triangle as a parametric line; call a
+       point on the line v. Compute v such that the z-component of [v]_B is 0.
    2c. Check if the x and y components of [v]_B are in [DELTA, 1-DELTA].
-3. Compute line of intersection between planes containing both triangles.
-4. Subdivide both triangles along line of intersection.
-   4a. For each subdivision:
-   4b. Transform the intersection into the basis of the triangle per above.
-   4c. Determine which two edges are intersected by the line.
-   4d. Create a mesh face from the triangle created by cutting along the line.
-   4e. Divide remaining quadrilateral into two triangular mesh faces.
-5. The existing mesh face that has been subdivided is reinserted into the list
+3. Subdivide both triangles along their line of intersection.
+   3a. For each subdivision:
+   3b. Find the line of intersection by projecting a (non-perpendicular) edge
+       of the other triangle onto the plane of the current triangle.
+   3c. Determine which two edges are intersected by the line.
+   3d. Create a mesh face from the triangle created by cutting along the line.
+   3e. Divide the remaining quadrilateral into two triangular mesh faces.
+4. The existing mesh face that has been subdivided is reinserted into the list
    without checks. The new mesh faces are inserted with this procedure.
    5a. If possible, skip checks for all polygons that have been checked before.
 
